1b83c3fa5151e151c8fd1fbdc09d48af2090c5ac
RAHUL SINGH RAWAT <121442029+RahulRawatdevops@users.noreply.github.com>
2023-11-23 10:59:33 +0530

    Update basicguessinggame.py
    edit the file suceesefully

--- basicguessinggame.py
+++ basicguessinggame.py
@@ -1,8 +1,18 @@
 import random
 def guess_the_number():
   print("Welcome to the number guessing game!")
+
+  play_again = True
+  while play_again:
+    
   secret_number = random.randint(1, 100)
   attempts = 0
+  max_attempts = 7
+  hint_after_attempt = 4
+
+while attempt < max_attempts:
+  user_guess = int(input("Guess the number (between 1 and 100: "))
+  attempts += 1
 
 while True:
   user_guess = int(input("Guess the number (between 1 and 100): "))
@@ -16,6 +26,14 @@ elif user_guess < secret_number:
 else:
   print("Too high! Try again.")
 
+if attempts == hint_after_attempt:
+  print(f"Hint: The number is between {secret_number -10} and {secret_number + 10}.)
+
+if attempts == max_attempts:
+ print(f"sorry, you have reached the maximum attempts. The correct numbaer was {secret number}."
+
+play_again_input = input("Do you want to play again? (yes/no): " ).lower()
+play_again == play_again_input == 'yes'
 
 if __name__ == "__main__":
   guess_the_number()
